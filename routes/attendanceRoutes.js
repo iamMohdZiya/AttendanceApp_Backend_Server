8a33aea5a2b03f7d260353f26d739ebb1b534531
Mohd. Ziya <mzziya2004@gmail.com>
2025-11-24 00:47:51 +0530

    attendece app

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { markAttendance, getStudentHistory, getSessionAttendance } = require('../controllers/attendanceController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, facultyOnly } = require('../middleware/authMiddleware');
 
 // All these routes need the user to be logged in
 router.post('/mark', protect, markAttendance);
