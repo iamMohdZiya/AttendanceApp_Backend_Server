b9dab8ad2aee2573c9d73a9a7ada53dea4c4bf11
Mohd. Ziya <mzziya2004@gmail.com>
2025-11-24 19:09:32 +0530

    update bulk attendence

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser,updateUserProfile,registerBulkUsers } = require('../controllers/authController');
-
+const { protect } = require('../middleware/authMiddleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.put('/profile', protect, updateUserProfile);
